edf4b5a1faa65b49a11959c2917cc2ef0991f09d
fcul <fcul804@aucklanduni.ac.nz>
2024-09-11 13:23:42 +1200

    changed comments in main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -16,7 +16,7 @@ public class Main {
         double[] resultForStudy1 = Study1.main(originalData);
         System.out.println(Arrays.toString(resultForStudy1));
 
-        //inputs for study1 and study2. Please change according to each question
+        //inputs for study3 and study4. Please change according to each question
         Study3 study3 = new Study3();
         String resultForStudy3 = study3.main(1, study3.FEBRUARY,1900);
         System.out.println(resultForStudy3);
